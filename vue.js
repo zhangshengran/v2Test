9c99bd58b7ba4908d50904812efce390951b9756
zhangshengran <zhangshengran@xiaomi.com>
2022-02-09 16:36:37 +0800

    3

--- vue.js
+++ vue.js
@@ -4,7 +4,7 @@ export class Vue{
     // _proxy.call(this,options.data)
     _proxy.call(this, options.data);/*构造函数中*/
     observe( this._data,options.render)
-    
+    let watcher = new Watcher(this,)
   }
 }
 
@@ -15,18 +15,20 @@ function observe(value,cb){
 }
 
 function defineReactive(obj,key,val,cb){
+    let dep = new Dep()
   Object.defineProperty(obj,key,{
     enumerable:true,
     configurable:true,
     get(){
       // 收集依赖
-      // debugger
-      return obj[key]
-      // return val;
+      if(Dep.target){
+        dep.addSub(Dep.target)
+      }
+      return val
     },
     set:(newVal)=>{
       val = newVal
-      cb()
+     dep.notify()
     }
   })
 }
@@ -46,4 +48,31 @@ function _proxy(data){
       }
     })
   })
+}
+
+class Watcher{
+  constructor(vm,expOrFn,cb,options){
+    this.cb = cb
+    this.vm = vm
+   Dep.target = this
+  //  this.cb.call(this.vm)
+  }
+
+  update(){
+    debugger
+    // this.cb.call(this.vm)
+  }
+}
+class Dep{
+  constructor(){
+    this.subs = []
+  }
+  addSub(sub){
+    this.subs.push(sub)
+  }
+  notify(){
+    this.subs.forEach((sub)=>{
+        sub.update()
+    })
+  }
 }
