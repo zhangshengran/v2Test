ae860f8969f9b994d0e5fa229caf8a1e6672e816
zhangshengran <zhangshengran@xiaomi.com>
2022-02-16 11:54:57 +0800

    vnode渲染

--- minivue/App.js
+++ minivue/App.js
@@ -1,15 +1,50 @@
 import { reactive, effectWatch } from './reactivity/index.js'
 
+function mountElement(vnode, element) {
+  const { tag, props, children } = vnode
+  // 创建元素
+  let ele = document.createElement(tag)
+  // 设置props
+  if (props) {
+    Object.keys(props).forEach((key) => {
+      const value = props[key]
+      ele.setAttribute(key, value)
+    })
+  }
+  if (children) {
+    if (typeof children === 'string') {
+      ele.innerHTML = `${children}`
+    }
+    if (Array.isArray(children)) {
+      // 递归mount
+      children.forEach((childVnode) => {
+        mountElement(childVnode, ele)
+      })
 
+    }
+  }
+  if (element) {
+    element.appendChild(ele)
+  } else {
+    return ele
+  }
+
+}
+
+function h(tag, props, children) {
+  return {
+    tag, props, children
+  }
+}
 export default {
   render(context) {
+    // 创建vnode
+    let vNode = h('div', null, [h('div', { id: 123 }, String(context.state.ddd.aaa))])
+    console.log('vNode', vNode)
     // debugger
-    effectWatch(() => {
-      let body = document.querySelector('body')
-      // let node = createNode()
-      // mountElement()
-      body.innerHTML = ` ${String(context.state.ddd.aaa)}`
-    })
+    // 渲染vnode
+    let element = mountElement(vNode)
+    return element
   },
   setup() {
     let ddd = window.ddd = reactive({ aaa: 123 })
--- minivue/index.js
+++ minivue/index.js
@@ -1,14 +1,20 @@
 import App from './App.js'
-App.render(App.setup())
-// function createApp(App) {
-//   return {
-//     mount(container) {
-//       let context = App.setup()
-//       // debugger
-//       let node = App.render(context)
-//       // document.querySelector(container).appendChild(node)
-//     }
-//   }
-// }
+import { reactive, effectWatch } from './reactivity/index.js'
+// App.render(App.setup())
+function createApp(App) {
+  return {
+    mount(container) {
+      let context = App.setup()
+      // debugger
+      effectWatch(() => {
+        let rootEle = document.querySelector(container)
+        rootEle.innerHTML = ``
+        let element = App.render(context)
+        rootEle.appendChild(element)
+      })
 
-// createApp(App).mount('#app')
+    }
+  }
+}
+
+createApp(App).mount('#app')
