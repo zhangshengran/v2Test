a3a8cbc770b36c86bd6cc65aaee6bcef3176bfbb
zhangshengran <zhangshengran@xiaomi.com>
2022-02-23 14:47:41 +0800

    搭建tdd环境完成

--- minivue/core/createApp.js
+++ minivue/core/createApp.js
@@ -1,15 +1,63 @@
 import { reactive, effectWatch } from '../reactivity/index.js'
 // App.render(App.setup())
+
+
+function mountElement(vnode, element) {
+  const { tag, props, children } = vnode
+  // 创建元素
+  let ele = vnode.el = document.createElement(tag)
+  // 设置props
+  if (props) {
+    Object.keys(props).forEach((key) => {
+      const value = props[key]
+      ele.setAttribute(key, value)
+    })
+  }
+  if (children) {
+    if (typeof children === 'string') {
+      ele.innerHTML = `${children}`
+    }
+    if (Array.isArray(children)) {
+      // 递归mount
+      children.forEach((childVnode) => {
+        mountElement(childVnode, ele)
+      })
+
+    }
+  }
+  if (element) {
+    element.appendChild(ele)
+  } else {
+    return ele
+  }
+
+}
+
+
 export function createApp(App) {
   return {
     mount(container) {
       let context = App.setup()
       // debugger
+      let isMounted = false
+      let preSubTree
       effectWatch(() => {
         let rootEle = document.querySelector(container)
         rootEle.innerHTML = ``
-        let element = App.render(context)
-        rootEle.appendChild(element)
+
+        let subTree = App.render(context)
+        preSubTree = subTree
+        // 渲染vnode
+        if (!isMounted) {
+          // 第一次
+          isMounted = true
+          let element = mountElement(subTree)
+          rootEle.appendChild(element)
+        } else {
+          let subTree = App.render(context)
+          diff(preSubTree, subTree)
+        }
+
       })
 
     }
