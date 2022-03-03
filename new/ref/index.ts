7510a95ceb4b07e5c2cb53c1fef5c49415adcc1a
zhangshengran <zhangshengran@xiaomi.com>
2022-03-03 09:15:18 +0800

    ref基础

--- new/ref/index.ts
+++ new/ref/index.ts
@@ -3,6 +3,38 @@ export function ref(value) {
   let wrapper = {
     value: value
   }
+  Object.defineProperty(wrapper, '__v_isRef', { value: true })//设定一个标记且不可枚举
   let react = reactive(wrapper)
   return react
+}
+
+export function toRef(obj, key) {
+  let wrapper = {
+    get value() {
+      return obj[key]
+    },
+    set value(val) {
+      obj[key] = val
+    }
+  }
+  Object.defineProperty(wrapper, '__v_isRef', { value: true })//设定一个标记且不可枚举
+
+  return wrapper
+}
+
+export function toRefs(obj) {
+  let retObj = {}
+  Object.keys(obj).forEach((key) => {
+    retObj[key] = toRef(obj, key)
+  })
+  return proxyRefs(retObj)
+}
+// 自动脱ref
+export function proxyRefs(target) {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const value = target[key]
+      return value.__v_isRef ? value.value : value
+    }
+  })
 }
--- new/ref/ref.test.ts
+++ new/ref/ref.test.ts
@@ -1,13 +1,49 @@
-import { ref } from './index'
+import { ref, toRef, toRefs } from './index'
 import { effect } from '../effect'
-
+import { reactive } from '../reactive'
 test('ref基本', () => {
   let a = ref(1)
+  let num
   let spy = jest.fn(() => {
     console.log(a.value)
+    num = a.value
   })
   effect(spy)
   expect(spy).toBeCalledTimes(1)
   a.value++
   expect(spy).toBeCalledTimes(2)
+  expect(num).toBe(2)
 })
+
+
+test('toRef基本', () => {
+  let aa = reactive({ b: 1 })
+  let toR = toRef(aa, 'b')
+  let num: any = 0
+  let spy = jest.fn(() => {
+    console.log(toR.value)
+    num = toR.value
+  })
+  effect(spy)
+  aa.b++
+  expect(num).toBe(2)//响应式没丢失
+  toR.value++
+  expect(num).toBe(3)
+
+})
+
+
+test('toRefs', () => {
+  let aa = reactive({ a: 2, b: 1 })
+  let toR: any = { ...toRefs(aa) }//解构后响应式没丢失
+  let num: any = 0
+  let spy = jest.fn(() => {
+    console.log(toR.a)
+    num = toR.a //自动脱ref了  不用访问value
+  })
+  effect(spy)
+  aa.b++
+  expect(num).toBe(2)//响应式没丢失
+})
+
+
